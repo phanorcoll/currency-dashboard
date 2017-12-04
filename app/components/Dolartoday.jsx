65a1182cb4710bc2aaa259990e66833bfc9b0b55
Phanor Coll <phanorcoll@gmail.com>
2017-12-04 16:32:46 -0400

    added the base structure for the component, css grid layout and styles
    TODO: add graph

--- app/components/Dolartoday.jsx
+++ app/components/Dolartoday.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 export const DolarToday = () => (
-    <div>
-        <h1>Dolartoday info</h1>
+    <div className='grid'>
+        <div className='icon'>icon</div>
+        <div className='title'>DolarToday <span>(Price per U.S Dolar in Venezuela)</span></div>
+        <div className='info'>
+            <div className='date'>
+                Diciembre 4, 2017 09:33 AM
+            </div>
+            <div className='price'>
+                $ 103.024.27 Bs.F
+            </div>
+            <div className='price'>
+                € 122.598.88 Bs.F
+            </div>
+            <div className='trm'>
+                $TRM 3005.76 / 1 VEF = 0.025
+            </div>
+        </div>
+        <div className='graph'>graph</div>
     </div>
 );
